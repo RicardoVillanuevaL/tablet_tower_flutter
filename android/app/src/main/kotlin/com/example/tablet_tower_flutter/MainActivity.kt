395124b8b39d799812ee7b7bca5b3dd969b9f5d4
RicardoVillanuevaL <50005284+RicardoVillanuevaL@users.noreply.github.com>
2020-11-18 16:02:49 -0500

    generado app bundle y apk realease

--- android/app/src/main/kotlin/com/example/tablet_tower_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tablet_tower_flutter/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.tablet_tower_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
